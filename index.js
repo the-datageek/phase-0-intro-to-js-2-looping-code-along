4684b7b419e1747f5a0331c6a404ee9258a8a130
Faith Moraa <moraaf770@gmail.com>
2022-11-23 02:49:38 +0300

    Completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const names = [];
+
+
+function writeCards(names, event) {
+  for (let i = 0; i < names.length; i++) {
+    names[i] = (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return names;
+}
+writeCards(names);
+
+function countDown(num) {
+  let i = 0
+  while (i <= num) {
+    console.log(num--)
+
+  }
+}
